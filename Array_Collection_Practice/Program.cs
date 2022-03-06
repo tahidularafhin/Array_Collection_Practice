7cf284236ff1ae6a96677e0ec5dce10f71eb5f0a
tahidularafhin <48841466+tahidularafhin@users.noreply.github.com>
2022-03-06 10:58:15 +0100

    first commit

---
+++ Array_Collection_Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array_Collection_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Tahidul", "Habiba", "Tayeb", "Tithi" };
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
